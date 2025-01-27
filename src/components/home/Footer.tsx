2188522d86bb77686af85f21eaf8d98c0030c44c
Favour Emmanuel <femmaje@gmail.com>
2025-01-27 16:56:45 +0100

    code refactor

--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
   const handleJoinWaitList = useHandleWaitList();
 
   return (
-    <div className="flex max-w-[1140px] mx-auto mt-[59px] shadow-lg mr-[96px] ">
+    <div className="flex sm:hidden max-w-[1140px] mx-auto mt-[59px] shadow-lg mr-[96px] ">
       <div className="h-[180px] pl-[70px] pr-[42px]  mx-auto   flex gap-x-[246px] justify-center items-center">
         <div className="text-[32px] font-bold  leading-[42px] w-[585px]">
           Sigup to <span className="bg-[#5F63E1] font-normal text-white rounded-[40px] border-2 border-black px-[12px]">chowafrica</span> and say <br/>
