ba84b3fd4b500b09c1b62e6d9b97a42493bb125c
Favour Emmanuel <femmaje@gmail.com>
2025-01-28 14:08:30 +0100

    feat: mobile fix

--- src/components/ui/FoodGridGradient.tsx
+++ src/components/ui/FoodGridGradient.tsx
@@ -13,7 +13,11 @@ const FoodGridGradient = () => {
             } shadow-lg opacity-60 sm:min-w-[85.9px]  bg-white rounded-[7.65px] `}
           >
             {/* Image Section */}
-            <div className="bg-[#F4F3E8] max-w-[127.79px]  mx-[4.13px] mt-[4.13px] px-[20.5px] py-[14.37px] flex items-center justify-center rounded-lg ">
+            <div
+              className={`bg-[#F4F3E8] ${
+                (idx == 4 || idx == 8) && "md:px-[20.5px] md:py-[14.37px]"
+              }   mx-[4.13px] mt-[4.13px]   flex items-center justify-center rounded-lg`}
+            >
               <Image
                 src={meal.image}
                 alt={meal.title}
@@ -30,7 +34,7 @@ const FoodGridGradient = () => {
                     : meal.image
                 }
                 alt={meal.title}
-                className="md:hidden sm:w-[140px]"
+                className="md:hidden  sm:w-[200px]"
                 width={117.31}
                 height={117.31}
               />
